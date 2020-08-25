4e05e0d0afa0a27b697ed263034e1801e18ae675
Documenter.jl <documenter@juliadocs.github.io>
2020-08-25 02:38:37 +0000

    build based on 6c19e95

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultivariateMedians","category":"page"},{"location":"#MultivariateMedians","page":"Home","title":"MultivariateMedians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultivariateMedians]","category":"page"}]
+}
